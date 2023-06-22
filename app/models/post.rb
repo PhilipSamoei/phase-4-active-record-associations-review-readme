503d05f89d512885fc80e2cffa2844aa79766c7b
Philip Samoei <samphilkerich04@gmail.com>
2023-06-22 21:22:56 +0300

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
 class Post < ApplicationRecord
     belongs_to :author
-end
+    has_many :post_tags
+    has_many :tags, through: :post_tags
+  end
+  
+  class PostsTag < ApplicationRecord
+    belongs_to :post
+    belongs_to :tag
+  end
+  
+  class Tag < ApplicationRecord
+    has_many :post_tags
+    has_many :posts, through: :post_tags
+  end
